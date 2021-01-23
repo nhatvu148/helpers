3eae068d29ccf2f2a6a43f0b7438b0abfa04913b
Akiyama <nhatvu148@gmail.com>
2021-01-23 23:55:04 +0900

    add template

--- src/TestString.cpp
+++ src/TestString.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+
+template <class T>
+T sum(T a, T b)
+{
+    T result;
+    result = a + b;
+    return result;
+}
 
 int main()
 {
@@ -10,5 +19,23 @@ int main()
     std::cout << "What is your favorite team? ";
     std::getline(std::cin, mystr);
     std::cout << "I like " << mystr << " too!\n";
+
+    float price = 0;
+    int quantity = 0;
+
+    std::cout << "Enter price: ";
+    std::getline(std::cin, mystr);
+    std::stringstream(mystr) >> price;
+    std::cout << "Enter quantity: ";
+    std::getline(std::cin, mystr);
+    std::stringstream(mystr) >> quantity;
+    std::cout << "Total price: " << price * quantity << std::endl;
+
+    int i = 5, j = 6, k;
+    double f = 2.0, g = 0.5, h;
+    k = sum<int>(i, j);
+    h = sum<double>(f, g);
+    std::cout << k << '\n';
+    std::cout << h << '\n';
     return 0;
 }
