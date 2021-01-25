ec78c515b03d483de57d7e75c5c9ae1a8d1c9928
Akiyama <nhatvu148@gmail.com>
2021-01-25 15:07:09 +0900

    read binary

--- src/ReadText.cpp
+++ src/ReadText.cpp
@@ -1,13 +1,58 @@
 #include <iostream>
 #include <fstream>
 
+#pragma pack(push, 1)
+
+struct Person
+{
+    char name[50];
+    int age;
+    double height;
+};
+
+#pragma pack(pop)
+
 int main()
 {
+    std::cout << sizeof(Person) << std::endl;
     std::string inFileName = "test.txt";
 
-    std::ifstream inFile;
+    Person someone = {"Nhat Vu", 30, 1.8};
+    std::string inBinName = "test.bin";
 
+    std::ifstream inFile;
     inFile.open(inFileName);
+
+    //// WRITE BINARY FILE ////
+    std::ofstream outFile;
+    outFile.open(inBinName, std::ios::binary);
+
+    if (outFile.is_open())
+    {
+        outFile.write(reinterpret_cast<char *>(&someone), sizeof(Person));
+        outFile.close();
+    }
+    else
+    {
+        std::cout << "Cannot create file: " << inBinName << std::endl;
+    }
+
+    //// READ BINARY FILE ////
+    Person someoneElse = {};
+
+    std::ifstream inputFile;
+    inputFile.open(inBinName, std::ios::binary);
+
+    if (inputFile.is_open())
+    {
+        inputFile.read(reinterpret_cast<char *>(&someoneElse), sizeof(Person));
+        inputFile.close();
+    }
+    else
+    {
+        std::cout << "Cannot read file: " << inBinName << std::endl;
+    }
+    std::cout << someoneElse.name << ", " << someoneElse.age << ", " << someoneElse.height << ", " << std::endl;
 
     if (inFile.is_open())
     {
@@ -21,7 +66,7 @@ int main()
             inFile >> population;
             // inFile.get();
             inFile >> std::ws;
-            
+
             if (!inFile)
             {
                 break;
