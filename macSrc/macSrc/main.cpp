06d66e3c0e73e0302eeca18b6122997a1522ae1c
nhatvu148 <nhatvu148@gmail.com>
2021-06-24 23:08:55 +0900

    add mac src

---
+++ macSrc/macSrc/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  macSrc
+//
+//  Created by NHAT VU on 6/24/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
