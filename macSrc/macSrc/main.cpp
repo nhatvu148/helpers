f497c538440b1e7742fdab70268ed0eed492b452
nhatvu148 <nhatvu148@gmail.com>
2021-06-24 23:29:41 +0900

    update main

--- macSrc/macSrc/main.cpp
+++ macSrc/macSrc/main.cpp
@@ -1,14 +1,34 @@
-//
-//  main.cpp
-//  macSrc
-//
-//  Created by NHAT VU on 6/24/21.
-//
-
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+// function declaration:
+double getAverage(int *arr, int size);
+
+int main()
+{
+    // an int array with 5 elements.
+    int balance[5] = {1000, 2, 3, 17, 50};
+    double avg;
+
+    // pass pointer to the array as an argument.
+    avg = getAverage(balance, 5);
+
+    // output the returned value
+    cout << "Average value is: " << avg << endl;
+
     return 0;
+}
+
+double getAverage(int *arr, int size)
+{
+    int i, sum = 0;
+    double avg;
+
+    for (i = 0; i < size; ++i)
+    {
+        sum += arr[i];
+    }
+    avg = double(sum) / size;
+
+    return avg;
 }
